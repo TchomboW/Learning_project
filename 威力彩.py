c7aa11625e5752e1153955e3654972dd55191353
TchomboW <79507838+TchomboW@users.noreply.github.com>
2021-03-23 14:45:10 +0800

    Add files via upload

---
+++ 威力彩.py
@@ -0,0 +1,13 @@
+import random
+
+lotto_numbers = set()
+sampleSize = 6
+answerSize = 0
+while answerSize < sampleSize:
+    r = random.randint(1,38)
+    if r not in lotto_numbers:
+        answerSize += 1
+        lotto_numbers.add(r)
+
+print(lotto_numbers)
+
